b3cf87b14915ab4d450e44689d837b9a46d08b4f
Jorge Bordarampé <jbordarampe@gmail.com>
2021-11-13 00:21:31 -0300

    Commmit func

--- js/funciones.js
+++ js/funciones.js
@@ -18,3 +18,54 @@
         }, false)
       })
   })()
+
+
+function permiso(){
+  if(calcularEdad()<23){
+    alert("Menor de Edad!!!")
+  }else{
+    alert("mayor de Edad!!!")
+
+  }
+}
+
+
+
+function calcularEdad() {
+
+    let mes=document.getElementById("mes");
+    let año=document.getElementById("año");
+    let dia=document.getElementById("dia");
+
+    var fechaActual = new Date();
+    var nacimiento = new Date(año.value,mes.value,dia.value)
+    var edad = fechaActual.getFullYear() - nacimiento.getFullYear();
+    var m = fechaActual.getMonth() - nacimiento.getMonth();
+
+    if (m < 0 || (m === 0 && fechaActual.getDate() < nacimiento.getDate())) {
+        edad--;
+    }
+    
+    return edad;
+}
+
+  
+
+
+ function cargarMeses() {
+    //Inicializamos el array.
+    var array = ["Enero", "Febrero", "Marzo", "Abril", "Mayo","Junio","Julio","Agosto","Septiembre", "Octubre","Noviembre","Diciembre"];
+    //Pasamos a la funcion addOptions(el ID del select, las provincias cargadas en el array).
+    addOptions("meses", array);
+}
+
+//Funcion para agregar opciones a un <select>.
+function addOptions(domElement, array) {
+  var selector = document.getElementsByName(domElement)[0];
+  //Recorremos el array.
+  for (val in array) {
+      var opcion = document.createElement("option");
+      opcion.text = array[val];
+      selector.add(opcion);
+  }
+}
